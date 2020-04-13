2aa9f6fb016859bb378fcd115cfb2691a090de0e
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2020-04-12 22:24:54 -0500

    Export definitions
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * @Author: Reinier Millo Sánchez <millo>
+ * @Date:   2020-04-12T21:24:31-05:00
+ * @Email:  reinier.millo88@gmail.com
+ * @Project: IKOABO Auth Microservice API
+ * @Filename: index.ts
+ * @Last modified by:   millo
+ * @Last modified time: 2020-04-12T22:23:30-05:00
+ * @Copyright: Copyright 2020 IKOA Business Opportunity
+ */
+
+/* Export api */
+export * from './api/Authenticator';
+/* Export types */
+export * from './types/errors';
