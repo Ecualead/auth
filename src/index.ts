d6aeadbd0dfe1fa5f85de08f25d66fd026e7f8ef
Reinier Millo Sánchez <millo@ikoabo.com>
2020-08-16 01:46:23 -0500

    Updating module exports
    Signed-off-by: Reinier Millo Sánchez <millo@ikoabo.com>

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 /**
- * @Author: Reinier Millo Sánchez <millo>
- * @Date:   2020-04-12T21:24:31-05:00
- * @Email:  reinier.millo88@gmail.com
- * @Project: IKOABO Auth Microservice API
- * @Filename: index.ts
- * @Last modified by:   millo
- * @Last modified time: 2020-04-12T22:23:30-05:00
- * @Copyright: Copyright 2020 IKOA Business Opportunity
+ * Copyright (C) 2020 IKOA Business Opportunity
+ * All Rights Reserved
+ * Author: Reinier Millo Sánchez <millo@ikoabo.com>
+ *
+ * This file is part of the IKOA Business Opportunity Auth API.
+ * It can't be copied and/or distributed without the express
+ * permission of the author.
  */
 
-/* Export api */
-export * from './api/Authenticator';
-/* Export types */
-export * from './types/errors';
+/* Export api middlewares */
+export { AuthenticationCtrl, IAuthentication } from './middlewares/authentication.middleware';
+
+/* Export models */
+export { AUTH_ERRORS } from './models/errors.enum';
