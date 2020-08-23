e5bbe47d0e86b91fc6ed22c72489219e73dd0035
Reinier Millo Sánchez <millo@ikoabo.com>
2020-08-23 02:45:02 -0500

    Update source code file header
    Signed-off-by: Reinier Millo Sánchez <millo@ikoabo.com>

--- src/middlewares/authentication.middleware.ts
+++ src/middlewares/authentication.middleware.ts
@@ -1,11 +1,8 @@
 /**
  * Copyright (C) 2020 IKOA Business Opportunity
- * All Rights Reserved
  * Author: Reinier Millo Sánchez <millo@ikoabo.com>
  *
  * This file is part of the IKOA Business Opportunity Auth API.
- * It can't be copied and/or distributed without the express
- * permission of the author.
  */
 import { HTTP_STATUS, Objects } from "@ikoabo/core";
 import { Request, Response, NextFunction } from "express";
