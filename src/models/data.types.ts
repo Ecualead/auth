e398a6598ae33fd8390311fb50610d1c6cd50b9c
Reinier Millo Sánchez <millo@ikoabo.com>
2021-07-18 02:14:11 -0500

    Add data type for account actions
    Signed-off-by: Reinier Millo Sánchez <millo@ikoabo.com>

---
+++ src/models/data.types.ts
@@ -0,0 +1,74 @@
+/**
+ * Copyright (C) 2020 - 2021 IKOA Business Opportunity
+ *
+ * All Rights Reserved
+ * Author: Reinier Millo Sánchez <millo@ikoabo.com>
+ *
+ * This file is part of the IKOA Business Oportunity Auth Package
+ * It can't be copied and/or distributed without the express
+ * permission of the author.
+ */
+
+export interface IAuthenticationResponse {
+  user: string;
+  username: string;
+  application: string;
+  project: string;
+  domain: string;
+  scope: string[];
+}
+
+export interface IRegisterData {
+  email: string;
+  password: string;
+  name?: string;
+  lastname1?: string;
+  lastname2?: string;
+  phone?: string;
+  referral?: string;
+  type?: number;
+  custom1?: string;
+  custom2?: string;
+}
+
+export interface ILoginData {
+  username: string;
+  password: string;
+}
+
+export interface IProfileUpdateData {
+  name?: string;
+  lastname1?: string;
+  lastname2?: string;
+  custom1?: string;
+  custom2?: string;
+}
+
+export interface IIdResponse {
+  id: string;
+}
+
+export interface ILoginResponse {
+  tokenType: string;
+  accessToken: string;
+  refreshToken: string;
+  accessTokenExpiresAt: number;
+  refreshTokenExpiresAt: number;
+  createdAt: number;
+  scope?: string[];
+}
+
+export interface IProfileResponse {
+  id: string;
+  name?: string;
+  lastname1?: string;
+  lastname2?: string;
+  code?: string;
+  initials?: string;
+  color1?: string;
+  color2?: string;
+  referral?: string;
+  type?: number;
+  custom1?: string;
+  custom2?: string;
+}
