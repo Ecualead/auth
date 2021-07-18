01a4b42103e4a10634f3ff1dbbc57b256235564d
Reinier Millo Sánchez <millo@ikoabo.com>
2021-07-18 02:13:48 -0500

    Moving out the scope validation enum
    Signed-off-by: Reinier Millo Sánchez <millo@ikoabo.com>

---
+++ src/constants/scope.enum.ts
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C) 2020 - 2021 IKOA Business Opportunity
+ *
+ * All Rights Reserved
+ * Author: Reinier Millo Sánchez <millo@ikoabo.com>
+ *
+ * This file is part of the IKOA Business Oportunity Auth Package
+ * It can't be copied and/or distributed without the express
+ * permission of the author.
+ */
+export enum SCOPE_VALIDATION {
+  AND_VALIADTION = 1,
+  OR_VALIDATION = 2,
+  NOT_VALIDATION = 3
+}
