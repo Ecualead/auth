e5bf88c1664dc0102db0a68c1910077e96943c2b
Reinier Millo <millo@ikoabo.com>
2022-01-03 22:44:54 -0500

    Add token type constant
    Signed-off-by: Reinier Millo <millo@ikoabo.com>

---
+++ src/constants/user.enum.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) 2020 - 2022 ECUALEAD
+ *
+ * All Rights Reserved
+ * Author: Reinier Millo Sánchez <rmillo@ecualead.com>
+ *
+ * This file is part of the Developer Auth Package
+ * It can't be copied and/or distributed without the express
+ * permission of the author.
+ */
+ export enum OAUTH2_TOKEN_TYPE {
+    UNKNOWN = 0,
+    APPLICATION,
+    USER,
+    EXTERNAL_AUTH
+  }
+  
